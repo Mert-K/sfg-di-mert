0d6e4af17bf665018d71a7f461ea6311493bd35b
Mert Kurt <tyrael91@hotmail.com>
2024-10-27 02:26:31 +0300

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfg_di_mert/SfgDiMertApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg_di_mert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiMertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDiMertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfg_di_mert/SfgDiMertApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg_di_mert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiMertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
